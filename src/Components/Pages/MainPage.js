11dc19c83ce676545ae352709d6e34648e24751c
Dominik Pickenäcker <pickenecker.dominik@gmail.com>
2019-02-25 20:30:48 +0100

    Adds Link to SearchPage

--- src/Components/Pages/MainPage.js
+++ src/Components/Pages/MainPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Bookshelf from '../Bookshelf/Bookshelf';
 import * as APIfunctions from '../../BooksAPI';
 
@@ -83,7 +84,7 @@ class MainPage extends React.Component {
           </div>
         </div>
         <div className="open-search">
-          <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
