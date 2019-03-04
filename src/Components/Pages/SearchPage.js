138ce6008ec73aff35d24bcc1e3e1882c2e2c046
Dominik Pickenäcker <pickenecker.dominik@gmail.com>
2019-03-04 21:34:03 +0100

    Refactors SearchPage

--- src/Components/Pages/SearchPage.js
+++ src/Components/Pages/SearchPage.js
@@ -3,100 +3,95 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import * as APIfunctions from '../../BooksAPI';
-import Book from '../Bookshelf/Book';
+import Book from '../Bookshelf/Book'
 
 
 class SearchPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        query: '',
-        results: [],
-        isEmpty: false
+      query: '',
+      results: [],
+      isEmpty: false
     }
     this.updateBook = this.updateBook.bind(this);
   }
 
-    handleInputChange = (e) => {
-        this.setState({ query: e.target.value })
-        this.findBooks(e.target.value);
-    }
+  handleInputChange = (e) => {
+    this.setState({ query: e.target.value })
+    this.findBooks(e.target.value);
+  }
 
-    findBooks = (query) => {
-      if(query.trim() === '') {
-          this.setState({ results: [], isEmpty: false })
-          return;
-      }
+  findBooks = (query) => {
+    if(query.trim() === '') {
       this.setState({ results: [], isEmpty: false })
-
-      APIfunctions.search(query)
-          .then((response) => {
-              // check if the query is the same of the input value
-              const emptyResponse = !!response.error
-              const results = emptyResponse ? [] : response
-
-              // adding shelf properties
-              results.forEach(item => {
-                  const myBook = this.props.myBooks.find(elem => elem.id === item.id)
-                  if(myBook) item.shelf = myBook.shelf
-              })
-              this.setState({ results, isEmpty: emptyResponse })
-          });
+      return;
     }
+    this.setState({ results: [], isEmpty: false })
 
-    updateBook = (book, shelf) => {
-      APIfunctions.update(book, shelf)
-        .then(resp => {
-          book.shelf = shelf;
-          this.setState({ book });
-        });
-    }
+    APIfunctions.search(query)
+    .then((response) => {
+      // check if the query is the same of the input value
+      const emptyResponse = !!response.error
+      const results = emptyResponse ? [] : response
 
-    render () {
-      const isEmpty = this.state
+      // add the shelf-property t
+      results.forEach(item => {
+        const book = this.props.books.find((elem) => elem.id === item.id)
+        console.log(item)
 
-      return (
-        <div className="search-container">
-          <div className="search-books-bar">
-            <Link className="close-search" to="/">Close</Link>
-            <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by Title or Author" value={this.state.query}
-                onChange={(event) => this.handleInputChange(event)} />
-            </div>
+        if(book) {
+          item.shelf = book.shelf
+        }
+      })
+      this.setState({ results, isEmpty: emptyResponse })
+    });
+  }
+
+  updateBook = (book, shelf) => {
+    APIfunctions.update(book, shelf)
+      .then(resp => {
+        book.shelf = shelf;
+        this.setState({ book });
+      });
+  }
+
+  render () {
+    const isEmpty = this.state
+
+    return (
+      <div className="search-container">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+          <input type="text" placeholder="Search by Title or Author" value={this.state.query}
+            onChange={(event) => this.handleInputChange(event)} />
           </div>
-
-          <div className="message-container">
-            {isEmpty && (
-              'Your search did not return any results.'
-            )}
-            {this.state.results.length > 0 && (
-              `${this.state.results.length} results found...`
-            )}
-          </div>
-
-          <ol className="books-grid results">
-              {this.state.results.map((book) => (
-                <li key={book.id}>
-                  <Book 
-                    updateBook={this.updateBook}
-                    book={book}
-                    />
-                </li>
-              ))}
-          </ol>
         </div>
-      )
-    }
-}
 
-SearchPage.defaultProps = {
-    myBooks: [],
-    onUpdateBook: () => {}
-}
+        <div className="message-container">
+          {isEmpty && (
+            'Your search did not return any results.'
+          )}
+          {this.state.results.length > 0 && (
+            `${this.state.results.length} results found...`
+          )}
+        </div>
 
-SearchPage.propTypes = {
-    myBooks: PropTypes.array,
-    onUpdateBook: PropTypes.func
+        <ol className="books-grid results">
+          {this.state.results.map((book) => (
+            <li key={book.id}>
+              <Book 
+                updateBook={this.updateBook}
+                shelf={book.shelf}
+                book={book}
+                />
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
+  }
 }
 
 export default SearchPage;
