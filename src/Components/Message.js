7057772d89a02d30c0ca7a7c963f3d1be9e10b6d
Dominik Pickenäcker <pickenaecker.dominik@territory.de>
2019-03-07 09:28:12 +0100

    Pruning message.js

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -4,17 +4,9 @@ import Bookshelf from './Bookshelf/Bookshelf';
 import * as BooksAPI from '../BooksAPI';
 
 class Message extends React.Component {
-  state = {
-
-  }
-
-  componentDidMount(){
-
-  }
-
   render() {
-    return(
-      <div id='msg-popup-container' className='transition' style={{display: this.props.display}}>
+    return (
+      <div id='msg-popup-container' className='transition' style={{ display: this.props.display }}>
         <div id='msg-box' className='transition'>
           Book Added To: <strong><span id='msg-text'>{this.props.text}</span></strong>!
         </div>
